2c1a2b4a635b33777ef229576c6d8989526411e0
Vatsal Manot <vatsal.manot@yahoo.com>
2026-08-13 03:28:42 +0530

    Updated for release 0.1.0

--- Package.swift
+++ Package.swift
@@ -16,12 +16,13 @@ let package = Package(
         .target(
             name: "BareBonesBrowserKit_Aggregation",
             dependencies: ["BareBonesBrowserKit"],
+            packageAccess: false,
             swiftSettings: [.define("SCIPIO_PRECOMPILED_BINARY_WRAPPER")]
         ),
         .binaryTarget(
             name: "BareBonesBrowserKit",
-            url: "https://github.com/swift-precompiled/BareBonesBrowser/releases/download/0.1.0/BareBonesBrowserKit-a668eb7762700227ee525a8a50c533b13f8ba317f5bb1df3c80acf3aeddb6244.xcframework.zip",
-            checksum: "a668eb7762700227ee525a8a50c533b13f8ba317f5bb1df3c80acf3aeddb6244"
+            url: "https://github.com/swift-precompiled/BareBonesBrowser/releases/download/0.1.0/BareBonesBrowserKit-375c65236898e9b240530793732f2b6ae55df87a4d2fd3eea5824c5e83f4d1a4.xcframework.zip",
+            checksum: "375c65236898e9b240530793732f2b6ae55df87a4d2fd3eea5824c5e83f4d1a4"
         )
     ]
 )
